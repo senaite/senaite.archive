8618d962216ba7a6927ac16d3820d3dd4678740e
Jordi Puiggené <jp@naralabs.com>
2021-09-13 23:00:41 +0200

    Force a copy.deepcopy of uids when queueing new task

--- src/senaite/archive/adapters/queue.py
+++ src/senaite/archive/adapters/queue.py
@@ -18,6 +18,7 @@
 # Copyright 2021 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
+import copy
 from senaite.archive.config import QUEUE_TASK_ID
 from senaite.archive.interfaces import IArchiveFolder
 from senaite.archive.utils import queue_do_archive
@@ -50,7 +51,7 @@ class QueuedDoArchiveTaskAdapter(object):
         priority = task.get("priority", 50)
 
         # Process the objects, but one by one
-        uids = task.uids or []
+        uids = copy.deepcopy(task.uids) or []
         if uids:
             obj = api.get_object_by_uid(uids[0])
             doActionFor(obj, "archive")
